a77036371974d2a41466a9ea922c626580e54bc6
DI-AQUARIUS <aquarius@aquarius-air.local>
2022-05-08 14:41:02 +0900

    models.pyの編集: migration前

--- api/models.py
+++ api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model): # Profileモデル
+    user_prof = models.OneToOneField(
+        User,    # user_prof属性をUserと結びつけている。
+        related_name='profile',     # 逆参照の名称をprofileに変更。
+        on_delete=models.CASCADE    #
+    )
+    # friendsフィールド
+    friends = models.ManyToManyField(
+        User,
+        related_name='profiles_friends',    # Userモデルからfriend関係にあるUserにアクセスできる。
+        blank=True
+    )
+    friends_requests = models.ManyToManyField(
+        User,
+        related_name='profiles_friend_requests',
+        blank=True
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
